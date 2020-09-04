39f7085d991dd5f6164b6f85a9894de823e8e3bb
chenyueze <1536667332@qq.com>
2020-09-04 19:05:13 +0800

    "Ftest"

--- gitup.sh
+++ gitup.sh
@@ -1,5 +1,9 @@
-#!/bin/zsh 
+#!/bin/zsh
+echo \[Emma\] : git add .
 git add .
+echo \[Emma\] : git commit -m \"$1\"
 git commit -m \"$1\"
+echo \[Emma\] : git push
 git push
+echo \[Emma\] : over.
 #一键上传 github
