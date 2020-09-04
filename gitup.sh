41816404d5deea0ca9b2e5c253139ccaa1e8d192
chenyueze <1536667332@qq.com>
2020-09-04 19:13:17 +0800

    "update : add ERROR_MESSAGE"

--- gitup.sh
+++ gitup.sh
@@ -1,9 +1,17 @@
-#!/bin/zsh
-echo \[Emma\] : git add .
-git add .
-echo \[Emma\] : git commit -m \"$1\"
-git commit -m \"$1\"
-echo \[Emma\] : git push
-git push
-echo \[Emma\] : over.
-#一键上传 github
+#!/bin/zsh 
+
+if [[ $# = 0 ]]; then
+    echo usage : ./gitup.sh xxx
+else 
+	{
+        echo \[Emma\] : git add .
+        git add .
+        echo \[Emma\] : git commit -m \"$1\"
+        git commit -m \"$1\"
+        echo \[Emma\] : git push
+        git push
+        echo \[Emma\] : over.
+        #一键上传 github
+	}
+fi
+
